74c1f5ad34168eb064806a76bd830098b7194eb3
Amirsa025 <amirsaki06@gmail.com>
2022-09-20 15:30:48 +0430

    inital structure project Shopingfy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { store } from '../app/store';
+import {Provider} from "react-redux";
+import { ChakraProvider } from '@chakra-ui/react'
+//Import the extendTheme function
+import { extendTheme } from '@chakra-ui/react'
+//Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return<Provider store={store}>
+      <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+      </ChakraProvider>
+      </Provider>
+
 }
 
 export default MyApp
